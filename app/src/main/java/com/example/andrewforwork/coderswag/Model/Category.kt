e6a14e15f3a2f8932ff56e10407927618b3c31c0
Andrew Kiryukhin <andrewkir2001@mail.ru>
2018-03-07 19:44:43 +0300

    added adapted

--- app/src/main/java/com/example/andrewforwork/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/andrewforwork/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.example.andrewforwork.coderswag.Model
 /**
  * Created by AndrewForWork on 03.03.2018.
  */
-class Category(val title: String, val image:String)
+class Category(val title: String, val image:String){
+    override fun toString(): String {
+        return title
+    }
+}
